249677d541533fa42d3d9c5b2df7b1abc661bbb7
Samigirma1 <46662279+Samigirma1@users.noreply.github.com>
2019-10-25 19:05:20 -0400

    Formatting changes

--- lab08/monopoly.sql
+++ lab08/monopoly.sql
@@ -6,7 +6,7 @@
 -- 
 -- Student Name: Samuel Zeleke
 -- Section:      CS262-B
--- 				 Lab 08
+-- 		 Lab 08
 --
 
 -- Drop previous versions of the tables if they they exist, in reverse order of foreign keys.
@@ -69,45 +69,59 @@ INSERT INTO PlayerProperty VALUES (4, 'Villa', 'house', 40);
 
 
 ------------------------------------------------------------
----						Exercise 8.1				     ---
+---			Exercise 8.1		         ---
 ------------------------------------------------------------
+
 --a. Retrieve a list of all the games, ordered by date... --
+
 SELECT * 
 FROM Game 
 ORDER BY time DESC; 
+
 --b. Retrieve all the games that occurred in the past...  --
+
 SELECT *
 FROM Game
 WHERE  DATE_PART('day', CURRENT_TIMESTAMP - Game.time) < 7
 AND DATE_PART('day', CURRENT_TIMESTAMP - Game.time) >= 0;
+
 --c. Retrieve a list of players who have (non-NULL)...    --
+
 SELECT *
 FROM PlayerGame
 WHERE name IS NOT NULL;
+
 --d. Retrieve a list of IDs for players who have some...  --
+
 SELECT PlayerID
 FROM PlayerGame
 WHERE score > 2000;
+
 --e. Retrieve a list of players who have GMail accounts.  --
+
 SELECT *
 FROM Player
 WHERE emailAddress LIKE '%gmail%';
 
 ------------------------------------------------------------
----						Exercise 8.2				     ---
+---			Exercise 8.2		         ---
 ------------------------------------------------------------
 --a. Retrieve all “The King”’s game scores in decreasi... --
+
 SELECT score
 FROM PlayerGame, Player
 WHERE name = 'The King'
 AND Player.ID = PlayerGame.playerID
 ORDER BY score DESC;
+
 --b. Retrieve the name of the winner of the game playe... --
+
 SELECT Player.name
 FROM Player, PlayerGame, Game
 WHERE Game.time = '2006-06-28 13:20:00'
 AND Game.ID = PlayerGame.gameID
 AND Player.ID = PlayerGame.playerID;
+
 --c. So what does that P1.ID < P2.ID clause do in the...  --
 --
 --	ANSWER:
